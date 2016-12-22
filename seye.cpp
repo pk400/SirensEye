5c202ce55bf73282f96dd7756db422badca7f0d1
Joel Aro <joelaro@outlook.com>
2016-12-22 04:51:14 -0500

    Included stdint.h and switched structs to classes

--- seye.cpp
+++ seye.cpp
@@ -1,88 +1,65 @@
 #include "lodepng.h"
+#include <stdint.h>
 #include <iostream>
 
-struct Pixel {
-	Pixel() {};
-	Pixel(unsigned char tr, unsigned char tg, unsigned char tb, unsigned char ta)
-		: r(tr), g(tg), b(tb), a(ta) {	
-	}
-	unsigned char r;
-	unsigned char g;
-	unsigned char b;
-	unsigned char a;
-	void print() {
-		printf("%u %u %u %u\n", r, g, b, a);
-	}
-	/* 
-	 * Allows for easy RGBA value assignment to the pixel at coordinates x and y
-	 * Example usage: maze(x, y) = (unsigned char*)rgba
-	 */
-	Pixel& operator=(const unsigned char* rgba) {
-		r = rgba[0];
-		b = rgba[1];
-		g = rgba[2];
-		a = rgba[3];
-	}
-	bool operator==(const Pixel& rgba) {
-		return r == rgba.r
-			&& g == rgba.g
-			&& b == rgba.b
-			&& a == rgba.a;
-	}
-} BLACK(255, 255, 255, 255), WHITE(0, 0, 0, 255);
-
-struct image {
-	unsigned height;
-	unsigned width;
-	std::vector<unsigned char> data;
-	Pixel** pixel;
-	/*
-	 * Allows for easy access to the pixel at coordinates x and y
-	 * Example usage: maze(x, y).print()
-	 */
-	Pixel& operator()(int x, int y) {
-		return pixel[x][y];
-	}
-} maze;
-
-/*
- * pixelify() initializes a grid populated with pixels
- */
-void pixelify() {
-	int idx = 0; // Keeps track of the current position in the maze.data vector
-
-	maze.pixel = new Pixel*[maze.width];
-	for(int x = 0; x < maze.width; x++) {
-		maze.pixel[x] = new Pixel[maze.height];
-		for(int y = 0; y < maze.height; y++) {
-			unsigned char rgba[4] = {
-				maze.data[idx++], maze.data[idx++],
-				maze.data[idx++], maze.data[idx++]
-			};
-
-			// Allocates RGBA values for pixels at coordinates x and y
-			maze(x,y) = rgba;
-			if(maze(x, y) == WHITE) {
-				std::cout << "#";
-			}
-			else {
-				std::cout << " ";
-			}
-			//maze(x, y).print();
+class Pixel {
+	public:
+		Pixel() {}
+		Pixel(uint16_t r, uint16_t g, uint16_t b, uint16_t a) 
+			: r_(r), g_(g), b_(b), a_(a) {}
+		std::vector<uint16_t> getRGBA() {
+			return { r_, g_, b_, a_ };
 		}
-		std::cout << std::endl;
-	}
-}
+		Pixel& operator=(const uint16_t* rgba) {
+			r_ = rgba[0];
+			g_ = rgba[1];
+			b_ = rgba[2];
+			a_ = rgba[3];
+		}
+		bool operator==(const Pixel*& rgba) {
+			return 
+				r_ == rgba->r_ && 
+				g_ == rgba->g_ && 
+				b_ == rgba->b_ &&
+				a_ == rgba->a_;
+		}
+		void printt() {
+			std::cout << r_ << " " << g_ << " " << b_ << " " << a_ << std::endl;
+		}
+	private:
+		uint16_t r_, g_, b_, a_;
+};
 
-void decodePNG(const char* filename) {
-	unsigned error = lodepng::decode(maze.data, maze.width, maze.height, filename);
+class Image {
+	public:
+		Image(const char* filename) {
+			std::vector<uint8_t> rawData;
+			unsigned error = lodepng::decode( rawData, width_, height_, filename);
+			if(error) std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
 
-	if(error) std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
-}
+			for(uint16_t i = 0; i < rawData.size(); i+=4) {
+				data_.push_back(new Pixel(
+						rawData[i],
+						rawData[i+1], 
+						rawData[i+2], 
+						rawData[i+3]));
+			}
+		}
+		Pixel* operator()(int x, int y) {
+			return data_[y * width_ + x];
+		}
+		uint16_t getWidth() { return width_; }
+		uint16_t getHeight() { return height_; }
+	private:
+		unsigned int width_;
+		unsigned int height_;
+		std::vector<Pixel*> data_;
+};
 
 int main(int argc, char* argv[]) {
 	const char* filename = argc > 1 ? argv[1] : "maze.png";
 
-	decodePNG(filename);
-	pixelify();	
+	Image maze(filename);
+
+	//pixelify();	
 }
